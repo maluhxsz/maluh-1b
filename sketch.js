90ff5a7c1be983266404b828c25b20b3486f0eae
maluhxsz <souza.silva.maria2707@escola.pr.gov.br>
2024-08-26 11:34:36 -0300

    Create sketch.js

---
+++ sketch.js
@@ -0,0 +1,16 @@
+function setup() {
+  createCanvas(400, 400)
+}
+
+function draw() {
+  background(220);
+ fill ("black");
+  let palavra= "MALU";
+  textSize(64);
+  textAlign(CENTER, CENTER);
+text (palavra, 200, 200);
+
+}     
+   if (mouseX)
+     
+   
